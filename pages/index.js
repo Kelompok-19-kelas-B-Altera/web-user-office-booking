d391575cbf0aa780c7620c3b16b72887409bb355
dutamfr30 <dutamfr@gmail.com>
2022-06-20 13:24:29 +0800

    create building-card component

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,73 @@
 import LiveChat from '../components/LiveChat'
+import CardBuilding from "../components/CardBuilding";
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={`${styles.container} relative bg-slate-600 w-full h-screen bg-secondary`}>
+    <div className={`${styles.container} relative bg-slate-600 pl-10 pr-40`}>
       <LiveChat />
-      User Office Booking
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic perspiciatis minus veniam minima quaerat corporis ex officiis natus reprehenderit beatae.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic perspiciatis minus veniam minima quaerat corporis ex officiis natus reprehenderit beatae.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic perspiciatis minus veniam minima quaerat corporis ex officiis natus reprehenderit beatae.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic perspiciatis minus veniam minima quaerat corporis ex officiis natus reprehenderit beatae.
-      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Hic perspiciatis minus veniam minima quaerat corporis ex officiis natus reprehenderit beatae.
+      <div className="">
+        <h1 className="font-semibold text-3xl mb-8">Banyak dilihat</h1>
+        <div className={`${styles.listBuilding} flex justify-between`}>
+          <CardBuilding 
+            buildingImage={"/building1.svg"}
+            rating={"4.7"}
+            buildingName={"Sarana Square"}
+            buildingLocation={["Tebet", "Jakarta Selatan"]}
+          />
+          <CardBuilding 
+            buildingImage={"/building1.svg"}
+            rating={"4.7"}
+            buildingName={"Sarana Square"}
+            buildingLocation={["Tebet", "Jakarta Selatan"]}
+          />
+        </div>
+      </div>
+      <div>
+        <h1 className="font-semibold text-3xl mb-6 mt-9">Rekomendasi</h1>
+        <div className={`${styles.listBuilding} flex justify-between mb-6`}>
+        <CardBuilding 
+            buildingImage={"/building1.svg"}
+            rating={"4.7"}
+            buildingName={"Sarana Square"}
+            buildingLocation={["Tebet", "Jakarta Selatan"]}
+          />
+          <CardBuilding 
+            buildingImage={"/building1.svg"}
+            rating={"4.7"}
+            buildingName={"Sarana Square"}
+            buildingLocation={["Tebet", "Jakarta Selatan"]}
+          />
+        </div>
+        <div className={`${styles.listBuilding} flex justify-between mb-6`}>
+          <CardBuilding 
+            buildingImage={"/building1.svg"}
+            rating={"4.7"}
+            buildingName={"Sarana Square"}
+            buildingLocation={["Tebet", "Jakarta Selatan"]}
+          />
+          <CardBuilding 
+            buildingImage={"/building1.svg"}
+            rating={"4.7"}
+            buildingName={"Sarana Square"}
+            buildingLocation={["Tebet", "Jakarta Selatan"]}
+          />
+        </div>
+        <div className={`${styles.listBuilding} flex justify-between mb-6`}>
+          <CardBuilding 
+            buildingImage={"/building1.svg"}
+            rating={"4.7"}
+            buildingName={"Sarana Square"}
+            buildingLocation={["Tebet", "Jakarta Selatan"]}
+          />
+          <CardBuilding 
+            buildingImage={"/building1.svg"}
+            rating={"4.7"}
+            buildingName={"Sarana Square"}
+            buildingLocation={["Tebet", "Jakarta Selatan"]}
+          />
+        </div>
+      </div>
     </div>
   )
 }
