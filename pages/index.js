70aa3a2de5096865bb43f68e4534375b917daa36
nibnualdi <nibnualdi@gmail.com>
2022-06-27 19:52:40 +0700

    add: integration user api

--- pages/index.js
+++ pages/index.js
@@ -7,18 +7,63 @@ import Search from "../components/SearchFeature/Search";
 import Filter from "../components/FilterFeature/Filter";
 import FilterByName from "../components/FilterByTime";
 import Cookies from "js-cookie";
+import { decodeToken } from "react-jwt";
 
 export default function Home() {
   const token = Cookies.get("token");
+  const tokenDecoded = decodeToken(token);
+
+  console.log(token);
 
   return (
     <div className="flex flex-col items-center" style={{ width: "100%" }}>
       <LiveChat />
       <section className={`${styles.sideBarContainer} fixed left-0 top-0 flex justify-center z-40`}>
         <div className="flex flex-col gap-5 items-center text-center">
+          {/* <div className="flex flex-col justify-center items-center"> */}
+            {/* {token ? (
+              <>
+                <Link href={`/profile/${tokenDecoded.email}`}>
+                  <a>
+                    <img
+                      src="/building.svg"
+                      alt="profile"
+                      width={137}
+                      height={137}
+                      className="w-[137px] h-[137px] rounded-full object-cover mb-3.5"
+                    />
+                  </a>
+                </Link>
+                <Link href={`/profile/${tokenDecoded.email}`}>
+                  <a className="text-base text-blue capitalize">{tokenDecoded.fullname}</a>
+                </Link>
+              </>
+            ) : (
+              <>
+                <p className="text-xl mb-3">Anda belum masuk</p>
+                <div className="flex gap-2">
+                  <Link href="/login">
+                    <a>
+                      <button className="w-[90px] h-[36px] bg-blue text-white text-sm rounded-sm">
+                        Masuk
+                      </button>
+                    </a>
+                  </Link>
+                  <Link href="/signup">
+                    <a>
+                      <button className="w-[90px] h-[36px] border border-blue text-blue text-sm rounded-sm">
+                        Daftar
+                      </button>
+                    </a>
+                  </Link>
+                </div>
+              </>
+            )}
+          </div> */}
+
           {token ? (
-            <div>
-              <Link href="/profile">
+            <div className="flex flex-col justify-center items-center">
+              <Link href={`/profile/${tokenDecoded.email}`}>
                 <a>
                   <img
                     src="/building.svg"
@@ -29,8 +74,8 @@ export default function Home() {
                   />
                 </a>
               </Link>
-              <Link href="/profile">
-                <a className="text-base text-blue capitalize">name</a>
+              <Link href={`/profile/${tokenDecoded.email}`}>
+                <a className="text-base text-blue capitalize">{tokenDecoded.fullname}</a>
               </Link>
             </div>
           ) : (
@@ -68,12 +113,16 @@ export default function Home() {
           <div className="" style={{ marginTop: "41px" }}>
             <h1 className="font-semibold text-3xl mb-8">Banyak dilihat</h1>
             <div className={`${styles.listBuilding} flex justify-between`}>
-              <CardBuilding
-                buildingImage={"/building1.svg"}
-                rating={"4.7"}
-                buildingName={"Sarana Square"}
-                buildingLocation={["Tebet", "Jakarta Selatan"]}
-              />
+              <Link href="/detail/2">
+                <a>
+                  <CardBuilding
+                    buildingImage={"/building1.svg"}
+                    rating={"4.7"}
+                    buildingName={"Sarana Square"}
+                    buildingLocation={["Tebet", "Jakarta Selatan"]}
+                  />
+                </a>
+              </Link>
               <CardBuilding
                 buildingImage={"/building1.svg"}
                 rating={"4.7"}
