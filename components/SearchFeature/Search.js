3b6683acb7d3d6901cab26f7882bea775a28ec56
nibnualdi <nibnualdi@gmail.com>
2022-07-06 04:32:48 +0700

    add: list building on search feature

--- components/SearchFeature/Search.js
+++ components/SearchFeature/Search.js
@@ -1,8 +1,16 @@
 import React from "react";
+import { useState } from "react";
 import { useRef } from "react";
 
 const Search = () => {
   const ref = useRef(null);
+  let [isFocus, setIsFocus] = useState(false);
+
+  const handleOnBlur = (e) => {
+    if (e.relatedTarget?.name !== "building-list") {
+      setIsFocus(false);
+    }
+  };
 
   return (
     <div>
@@ -13,14 +21,33 @@ const Search = () => {
             ref.current.focus();
           }}
         >
-          <img src="/search.svg" alt="search" className="absolute left-2.5 top-2.5" />
+          <img src="/search.svg" alt="search" className="absolute left-2.5 top-2.5 z-20" />
           <input
             type="text"
             ref={ref}
-            className="pl-[50px] pr-4 py-[12.5px] w-[352px] focus:outline-secondary"
+            className="pl-[50px] pr-4 py-[12.5px] w-[352px] focus:outline-secondary absolute z-10"
             placeholder="Search..."
             onChange={(e) => console.log(e.target.value.toLowerCase())}
+            onFocus={() => {
+              setIsFocus(true);
+            }}
+            onBlur={(e) => {
+              handleOnBlur(e);
+            }}
           />
+
+          {isFocus && (
+            <div className="absolute w-[352px] h-[256px] overflow-hidden bg-white z-[1] pt-[51px]">
+              {/* this is the list */}
+              <button
+                name="building-list"
+                className="w-[352px] h-[39px] p-[10px] text-[#07072380] font-normal text-base text-left"
+              >
+                Building Name
+              </button>
+              {/* this is the list */}
+            </div>
+          )}
         </div>
       </div>
     </div>
