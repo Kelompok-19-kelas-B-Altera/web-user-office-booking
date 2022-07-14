d69d6e9d4b4b47515545b778f75bd10cb5f6ed8d
nibnualdi <nibnualdi@gmail.com>
2022-07-14 22:38:08 +0700

    fix: import axiosInstance in filter component

--- components/FilterFeature/Filter.js
+++ components/FilterFeature/Filter.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import axiosInstance from "../networks/apis";
+import axiosInstance from "../../networks/apis";
 import React, { useEffect, useState } from "react";
 import ListItemFilter from "./ListItem";
 
