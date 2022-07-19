4a87900a1f236c0b51df2baea4779387c585c3be
nibnualdi <nibnualdi@gmail.com>
2022-07-19 21:14:50 +0700

    fix: graphql.js

--- networks/graphql/index.js
+++ networks/graphql/index.js
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider, gql } from "@apollo/client
 const isDev = process.env.NODE_ENV === "development";
 
 const client = new ApolloClient({
-  uri: `${isDev && process.env.NEXT_PUBLIC_BASE_URL}/graphql`,
+  uri: `${isDev ? process.env.NEXT_PUBLIC_BASE_URL : ""}/graphql`,
   cache: new InMemoryCache(),
 });
 
